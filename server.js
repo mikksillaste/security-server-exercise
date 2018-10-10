e668db049db9eb4597d6a64c03d1ffd107be5ecb
mikksillaste <mikk.sillaste@gmail.com>
2018-10-10 22:50:59 +0300

    video 214(0:00)

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-const express = require('express')
-const cors = require('cors')
-const helmet = require('helmet')
-var winston = require('winston');
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const winston = require('winston');
+const morgan = require('morgan');
 
 const bodyParser = require('body-parser');
-const app = express()
-app.use(cors())
-app.use(helmet())
-app.use(bodyParser.json())
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
@@ -21,6 +23,6 @@ app.post('/secret', (req, res) => {
     winston.error('This guy is messing with us:' + userInput);
     res.status(400).json('incorrect submission')
   }
-})
+});
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
