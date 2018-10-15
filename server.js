c1dd9031920709f2a50dde1860b2cb560b19b6e0
mikksillaste <mikk.sillaste@gmail.com>
2018-10-15 07:38:54 +0300

    video 221(3:19)

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
 app.get('/', (req, res) => {
-  res.cookie('session', '1', { httpOnly: true });
+/*  res.cookie('session', '1', { httpOnly: true });
   res.cookie('session', '1', { secure: true });
   res.set({
       'Content-Security-Policy': "script-src 'self' 'https://apis.google.com'"
-  });
+  });*/
   res.send('Hello World!')
 });
 
